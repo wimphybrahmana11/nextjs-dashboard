b9bcc501f4eabac2b93e5aee4e90e278930c6448
wimphybrahmana11 <wimphybrahmana@gmail.com>
2024-09-25 11:48:41 +0700

    ok

--- nextjs-dashboard/app/Dasboard/nav-links.tsx
+++ nextjs-dashboard/app/Dasboard/nav-links.tsx
@@ -10,7 +10,14 @@ import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
  
 // ...
- 
+const links = [
+  {name: 'Home', href:'/dashboard', icon: HomeIcon},
+  {name: 'Invoices',
+    href:'/dashboard/invoices', 
+    icon: DocumentDuplicateIcon},
+  {name: 'Customers', href:'/dashboard/customers', icon:  UserGroupIcon},
+  ];
+  
 export default function NavLinks() {
   const pathname = usePathname();
  
